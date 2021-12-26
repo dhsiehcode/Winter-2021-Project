078c4c9029202132ff750b97a4266623aa979e7a
dhsiehcode <89666559+dhsiehcode@users.noreply.github.com>
2021-12-26 16:50:25 -0500

    Added jsoup

--- Football Match Rating/src/DataQuery.java
+++ Football Match Rating/src/DataQuery.java
@@ -1,7 +1,11 @@
+import org.jsoup.Jsoup;
+import org.w3c.dom.Document;
+
 import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.annotation.Documented;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -11,28 +15,14 @@ public class DataQuery {
 
     public static void runQuery(){
         try {
-            System.out.println(getURLContent("https://www.transfermarkt.us/norwich-city/spielplan/verein/1123"));
+            Document doc = (Document) Jsoup.connect("https://www.transfermarkt.us/norwich-city/spielplan/verein/1123").get();
+
+            
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static String getURLContent(String inputURL) throws IOException {
-        StringBuilder content = new StringBuilder();
 
-        URL url = new URL(inputURL);
-
-        /*URLConnection connection = url.openConnection();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        String line;
-        while ((line = bufferedReader.readLine()) != null){
-            content.append(line + "\n");
-        }
-        bufferedReader.close();*/
-        Scanner sc = new Scanner(url.openStream());
-        while (sc.hasNext()){
-            content.append(sc.next());
-        }
-        return content.toString();
-    }
 }
